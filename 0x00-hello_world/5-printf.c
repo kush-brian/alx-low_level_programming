6a33edcdd0cf7e6f54b2b1daab287364b889b5ec
kush-brian <captainrian0@gmail.com>
2023-10-05 18:36:47 -0700

    c file 2

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ *
+ * Description: This is the main function that prints the message
+ *
+ * Return: Always returns 0 (successa)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
