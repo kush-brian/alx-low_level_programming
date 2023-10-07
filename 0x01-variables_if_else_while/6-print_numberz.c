31ee6f20a74caf6a5041349657b894b96f567943
kush-brian <captainrian0@gmail.com>
2023-10-06 19:26:37 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - te entry point
+ *
+ * Description: print numberz
+ *
+ * Return: returns 0 when successful
+ */
+
+int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		putchar('0' + i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
