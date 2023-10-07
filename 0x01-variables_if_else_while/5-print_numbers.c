8c5b41211bc2e537d413f9770caf839189dc6c0b
kush-brian <captainrian0@gmail.com>
2023-10-06 19:17:50 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point
+ *
+ * Description: print numbers
+ *
+ * Return: return 0 when successful
+ */
+
+int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+
+	return (0);
+}
