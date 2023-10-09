c2e727005c845b43e0e95f8d379f8c2ddc79a818
kush-brian <captainrian0@gmail.com>
2023-10-09 23:04:56 +0300

    another one

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point of the program
+ *
+ * Description: a printf program
+ *
+ * Return: returns 1 when successful
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
