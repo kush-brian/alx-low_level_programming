61f3616d6271c59cd23022c1f48ac5b78d626de4
kush-brian <captainrian0@gmail.com>
2023-10-11 17:31:36 +0300

    task one

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter ++;
+	}
+
+	putchar('\n');
+}
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
