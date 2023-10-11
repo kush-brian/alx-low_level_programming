7f98f0ad6b7ec863e19c0f0896f2232e7d167f7c
kush-brian <captainrian0@gmail.com>
2023-10-12 02:30:19 +0300

    task four

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - check if its alphabet
+ * @c: character to test
+ * Return: 1(True) else 0 (false)
+ */
+int _isalpha(char c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		if (c >= 'A' && c <= 'Z')
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
