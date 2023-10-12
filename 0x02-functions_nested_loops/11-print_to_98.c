5c2d0bb43e466d139ce11403e71b72b35d0ff53c
kush-brian <captainrian0@gmail.com>
2023-10-12 05:34:21 +0300

    multitasks

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,36 +1,33 @@
 #include "main.h"
 #include <stdio.h>
-/**
- * print_to_98 - prints all natural numbers
- *
- * @n: value to count from
- */
 
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: The starting number
+ */
 void print_to_98(int n)
 {
-	int i;
+    int i;
 
-	if (n == 98)
+    if (n <= 98)
+    {
+        for (i = n; i <= 98; i++)
 	{
-		printf("98");
-	}
-	else if (n < 98)
+            if (i == 98)
+                printf("%d\n", i);
+            else
+                printf("%d, ", i);
+        }
+    }
+    else
+    {
+        for (i = n; i >= 98; i--)
 	{
-		for (i = n; i <= 98; i++)
-		{
-			printf("%d", i);
-			if (i < 98)
-				_putchar(',');
-		}
-	}
-	else if (n > 98)
-	{
-		for (i = n; i >= 98; i--)
-		{
-			_putchar(i);
-			if (i > 98)
-				_putchar(',');
-		}
-	}
-	_putchar('\n');
+            if (i == 98)
+                printf("%d\n", i);
+            else
+                printf("%d, ", i);
+        }
+    }
 }
+
