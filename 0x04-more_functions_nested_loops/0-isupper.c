2da44d130a1639abaffe4849feab306a50b8401f
kush-brian <captainrian0@gmail.com>
2023-10-12 13:29:12 +0300

    task one and two

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if a number is uppercase
+ *
+ * Return: returns 1 if true, 0 otherwise
+ */
+
+int _isupper(char c)
+{
+
+	if (c = 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
