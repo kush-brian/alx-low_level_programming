8e57aa3a7dcdab45d246d43da41cce73d8ae0def
kush-brian <captainrian0@gmail.com>
2023-10-12 16:47:06 +0300

    task 4

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - product
+ * @a: first no
+ * @b: second no
+ * Return: the product of the numbers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+void print_numbers(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
