555406fa31b30a77a3002825fbf02f4fa7e3a9a1
kush-brian <captainrian0@gmail.com>
2023-10-19 18:24:17 +0300

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,25 +1,25 @@
 #include "main.h"
 /**
- * _strncpy - copy a string with a specified maximum length
+ * _strncat - copy a string with a specified maximum length
  * @dest: Destination string
  * @src: source string
  * @n: maximum no of characters to copy
  * Return: pointer to the resulting string
  */
 
-char *_strncpy(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 	int i;
+	int dest_len = 0;
+
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		dest[i] = src[i];
-	}
-
-	while (i < n)
-	{
-		dest[i] = '\0';
-		i++;
+		dest[dest_len + i] = src[i];
 	}
 
 	return (dest);
