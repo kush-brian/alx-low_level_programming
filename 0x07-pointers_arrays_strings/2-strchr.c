b98ce8ee664f2b71a6fa00bdb3cee9d6689d8b87
kush-brian <captainrian0@gmail.com>
2023-10-24 00:46:38 +0300

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * _strchr - locates a character in a string
@@ -19,5 +20,5 @@ char *_strchr(char *s, char c)
 		s++;
 	}
 
-	return ();
+	return (NULL);
 }
