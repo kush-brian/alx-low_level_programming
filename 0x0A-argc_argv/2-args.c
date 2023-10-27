e7887954c4cadf12d54cc37c9a617a0ad2797dd5
kush-brian <captainrian0@gmail.com>
2023-10-27 19:54:25 +0300

    task 1

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - returns arg count
+ * @argc: the count
+ * @argv: the vector
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+}
