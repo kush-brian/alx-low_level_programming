bbdcfc3e4a8dcd1f468dd82d3f982aa7bab90559
kush-brian <captainrian0@gmail.com>
2023-10-27 18:48:46 +0300

    task 1

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-ar rc liball.a *.o
+
+# Create a temporary directory to hold object files
+temp_dir=$(mktemp -d)
+
+# Compile all .c files in the current directory into object files
+for source_file in *.c; do
+    gcc -c "$source_file" -o "$temp_dir/${source_file%.c}.o"
+done
+
+# Create the static library liball.a from the object files
+ar rcs liball.a "$temp_dir"/*.o
+
+# Clean up the temporary directory
+rm -r "$temp_dir"
