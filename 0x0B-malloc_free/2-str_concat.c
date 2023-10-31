1cf7fc6f34b99fe2f737723aac32ba57fadb0b95
kush-brian <captainrian0@gmail.com>
2023-10-31 17:48:44 +0300

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,47 +15,38 @@ char *str_concat(char *s1, char *s2)
 	char *memory;
 
 	/* if no string is added */
-
 	if (s1 == NULL)
 	{
-		s1 = " ";
+		s1 = "";
 	}
 	if (s2 == NULL)
 	{
-		s2 = " ";
+		s2 = "";
 	}
-
-	/* calculate the length of both strings for optimal memory allocation */
-	while(s1[i] != '\0')
+	/* calc length of both strings for optimal memory allocation */
+	while (s1[i] != '\0')
 	{
 		i++;
 	}
-
 	while (s2[j] != '\0')
 	{
 		j++;
 	}
-
 	/* allocate the memory */
-
 	memory = (char *)malloc(sizeof(char) * (i + j + 1));
 	/* if the memory allocation fails */
 	if (memory == NULL)
 		return (NULL);
-
 	/* concanate the strings */
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		memory[i] = s1[i];
 	}
-
 	for (j = 0; s2[j] != '\0'; j++)
 	{
 		memory[i + j] = s2[j];
 	}
-
 	memory[i + j] = '\0';
-
 	return (memory);
 	free(memory);
 }
