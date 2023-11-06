0b43a6ea63e1393bf541bf99a3f67ebface11c10
kush-brian <captainrian0@gmail.com>
2023-11-06 20:06:25 +0300

    task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,9 +6,4 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d)i;
-typedef struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-}dog_t;
+typedef struct dog dog_t;
